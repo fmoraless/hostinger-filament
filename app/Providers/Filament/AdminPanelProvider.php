8a8ea1aeeec6a03c88d3bf7225d6197a79ee0e81
fmoraless <franticzero@gmail.com>
2024-03-19 13:33:26 -0300

    change logo and favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => 'rgb(103,76,196)',
             ])
             ->font('Poppins')
+            ->favicon('images/favicon.png')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,5 @@
+<img
+    src="{{ asset('images/Hostinger-logo.png')}}"
+    alt="Logo Hostinger"
+    class="h-16"
+>
